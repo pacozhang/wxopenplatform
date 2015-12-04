ece2d99175b39e69ad0763d16e9a3cca26e05ad7
pacozhhejm <pacozhhejm@aliyun.com>
2015-12-04 17:11:21 +0800

    Revert "微信开放平台"
    This reverts commit b107050963341c3c8a28100fa978825f183628b6.

--- src/nds/weixin/ext/phonemessage/ISendSMSMessage.java
+++
@@ -1,9 +0,0 @@
-package nds.weixin.ext.phonemessage;
-
-import nds.control.util.ValueHolder;
-
-import org.json.JSONObject;
-
-public interface ISendSMSMessage {
-	public ValueHolder sendmessage(JSONObject jo);
-}
